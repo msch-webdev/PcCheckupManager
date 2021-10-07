55873acd3fd3db88e7ba26d5213bd41708aed580
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-07 11:30:35 +0200

    Add require path

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,5 +1,6 @@
 const {app, BrowserWindow, ipcRenderer} = require('electron');
 const fs = require('fs');
+const pfad = require('path');
 const os = require('os');
 const si = require('systeminformation');
 
