1fd816fd8a5baf4fd62e426fe33bfde551038c7c
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-08 18:43:17 +0200

    Add CPU Infos

--- src/JS/index.js
+++ src/JS/index.js
@@ -4,3 +4,11 @@ const pfad = require('path');
 const os = require('os');
 const si = require('systeminformation');
 
+//Hohlt Daten der CPU
+const cpu = document.getElementById('cpu');
+si.cpu().then(data => {
+  let manufacturer = data.manufacturer;
+  let brand = data.brand;
+  cpu.value = `${manufacturer} ${brand}`;
+  console.log('CPU => ', cpu.value);
+})
