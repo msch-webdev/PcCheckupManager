e332b673dd423c94ea59024ed0e6f57bf977efbb
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-08 19:17:12 +0200

    Add todos

--- src/JS/index.js
+++ src/JS/index.js
@@ -4,11 +4,22 @@ const pfad = require('path');
 const os = require('os');
 const si = require('systeminformation');
 
+//todo: Hohlt das Aktuelle Datum
+//todo: Hohlt die Windows Version und die Update Version
+
 //Hohlt Daten der CPU
 const cpu = document.getElementById('cpu');
 si.cpu().then(data => {
   let manufacturer = data.manufacturer;
   let brand = data.brand;
-  cpu.value = `${manufacturer} ${brand}`;
+  let speed = data.speed;
+  let cores = data.cores;
+  cpu.value = `${manufacturer} ${brand} ( ${cores}x ${speed}GHz )`;
   console.log('CPU => ', cpu.value);
 })
+
+//todo: Hohlt die Ram Informationen
+//todo: Prüft ob HD oder SSD verbaut sind und wie viele
+//todo: Prüft welche Grafikkarte verbaut ist
+
+// todo: Prüft den Laufwerkspeicher verbrauch für jede platte
