fc3d3c84854290ae91007aad6953d48748e42410
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-09 15:39:40 +0200

    Add Date Time & OS datas

--- src/JS/index.js
+++ src/JS/index.js
@@ -4,8 +4,63 @@ const pfad = require('path');
 const os = require('os');
 const si = require('systeminformation');
 
-//todo: Hohlt das Aktuelle Datum
-//todo: Hohlt die Windows Version und die Update Version
+//Hohlt das Aktuelle Datum
+const heute = document.getElementById('datum');
+const date = new Date();
+const datum = (heute) => {
+  let monat = date.getMonth() + 1;
+  let tag = date.getDate();
+  let jahr = date.getFullYear();
+  heute.innerText = `${tag}.${monat}.${jahr}`;
+  return heute;
+};
+datum(heute);
+
+//Hohlt die Windows Version und die Update Version
+
+si.osInfo().then(data => {
+  const betriebsSystem = document.getElementById('os');
+  const builderArr = ['21H1', '20H2', '2004', '1909'];
+  let update = '';
+
+  // platform => 'linux', 'darwin', 'win32', ...
+  let platt = data.platform;
+
+  //distro => Windows 10 Pro
+  let distr = data.distro;
+
+  //arch => x64
+  let archi = data. arch;
+
+  //build => 19043 => 21H1
+  let buildVersion = data.build;
+  
+  if(buildVersion == 19043) {
+    update = builderArr[0];
+  }
+  else if(buildVersion == 19042) {
+    update = builderArr[1];
+  }
+  else if(buildVersion == 19041) {
+    update = builderArr[2];
+  }
+  else if(buildVersion == 18363) {
+    update = builderArr[3];
+  }
+  else {
+    update = 'n.a.'
+  }
+
+  // serial => "00330-51787-78469-AAOEM"
+  let serialNr = data.serial;
+
+  // uefi => true
+  let uefi = data.uefi;
+
+  betriebsSystem.innerText = `\n ${platt} => ${distr} - ${archi} Bit - \n Update Version: ${buildVersion} ( ${update} ) - UEFI: ${uefi} \n Serien Nr: ${serialNr}`;
+});
+
+
 
 //Hohlt Daten der CPU
 const cpu = document.getElementById('cpu');
