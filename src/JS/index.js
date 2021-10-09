b618a7278f85b0e7e5844c7159f76d08b6eaca1a
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-09 15:55:40 +0200

    clean code

--- src/JS/index.js
+++ src/JS/index.js
@@ -5,22 +5,21 @@ const os = require('os');
 const si = require('systeminformation');
 
 //Hohlt das Aktuelle Datum
-const heute = document.getElementById('datum');
-const date = new Date();
-const datum = (heute) => {
+const datum = () => {
+  const heute = document.getElementById('datum');
+  const date = new Date();
   let monat = date.getMonth() + 1;
   let tag = date.getDate();
   let jahr = date.getFullYear();
   heute.innerText = `${tag}.${monat}.${jahr}`;
   return heute;
 };
-datum(heute);
+datum();
 
 //Hohlt die Windows Version und die Update Version
-
 si.osInfo().then(data => {
   const betriebsSystem = document.getElementById('os');
-  const builderArr = ['21H1', '20H2', '2004', '1909'];
+  const builderWin = ['21H1', '20H2', '2004', '1909'];
   let update = '';
 
   // platform => 'linux', 'darwin', 'win32', ...
@@ -36,16 +35,16 @@ si.osInfo().then(data => {
   let buildVersion = data.build;
   
   if(buildVersion == 19043) {
-    update = builderArr[0];
+    update = builderWin[0];
   }
   else if(buildVersion == 19042) {
-    update = builderArr[1];
+    update = builderWin[1];
   }
   else if(buildVersion == 19041) {
-    update = builderArr[2];
+    update = builderWin[2];
   }
   else if(buildVersion == 18363) {
-    update = builderArr[3];
+    update = builderWin[3];
   }
   else {
     update = 'n.a.'
@@ -54,23 +53,19 @@ si.osInfo().then(data => {
   // serial => "00330-51787-78469-AAOEM"
   let serialNr = data.serial;
 
-  // uefi => true
-  let uefi = data.uefi;
-
-  betriebsSystem.innerText = `\n ${platt} => ${distr} - ${archi} Bit - \n Update Version: ${buildVersion} ( ${update} ) - UEFI: ${uefi} \n Serien Nr: ${serialNr}`;
+  betriebsSystem.innerText = `\n ${platt} => ${distr} - ${archi} Bit \n Update Version: ${buildVersion} ( ${update} ) \n Serien Nr: ${serialNr}`;
+  console.log('Betriebssystem => ', betriebsSystem);
 });
 
-
-
 //Hohlt Daten der CPU
-const cpu = document.getElementById('cpu');
 si.cpu().then(data => {
+  const cpu = document.getElementById('cpu');
   let manufacturer = data.manufacturer;
   let brand = data.brand;
   let speed = data.speed;
   let cores = data.cores;
-  cpu.value = `${manufacturer} ${brand} ( ${cores}x ${speed}GHz )`;
-  console.log('CPU => ', cpu.value);
+  cpu.innerText = `${manufacturer} ${brand} ( ${cores}x ${speed}GHz )`;
+  console.log('CPU => ', cpu);
 })
 
 //todo: Hohlt die Ram Informationen
