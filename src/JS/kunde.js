3f7545057b763e5afedcc448ca173db1b9d6fe62
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-10 17:23:21 +0200

    Add customer data infos

--- src/JS/kunde.js
+++ src/JS/kunde.js
@@ -3,12 +3,71 @@ const fs = require('fs');
 const pfad = require('path');
 
 
-//Eventlistener zum Speichern der Daten
+//Eventlistener zum Speichern der Daten in JSON
 const kundenSave = document.getElementById('kunde-save');
 kundenSave.addEventListener('click', () => {
-  const auftragsnr = document.getElementById('auftrag');
   //Hohlt die Auftragsnr.
+  const auftragsnr = document.getElementById('auftrag');
   if(auftragsnr.value !== '') {
-    console.log(auftragsnr.value);
+    console.log('Auftragsnr.: ',auftragsnr.value);
   }
-});
+  //Hohlt den Vornamen
+  const vorname = document.getElementById('vorname');
+  if(vorname.value !== '') {
+    console.log('Vorname: ', vorname.value);
+  }
+  //Hohlt den Nachnamen
+  const nachname = document.getElementById('nachname');
+  if(nachname.value !== '') {
+    console.log('Nachname: ',nachname.value);
+  }
+
+  //Hohlt den Anmelde Namen
+  const benutzerName = document.getElementById('benutzerName');
+  if(benutzerName.value !== '') {
+    console.log('Benutzername: ',benutzerName.value);
+  }
+
+  //Hohlt das Anmelde Passwort
+  const benutzerPW = document.getElementById('benutzerPW');
+  if(benutzerPW.value !== '') {
+    console.log('BenutzerPW: ',benutzerPW.value);
+  }
+
+  //Hohlt die OS daten
+  const os = document.getElementById('os');
+  if(os.innerText!== '') {
+    console.log('OS: ',os.innerText);
+  }
+
+  // Hohlt die CPU daten
+  const cpu = document.getElementById('cpu');
+  if(cpu.innerText!== '') {
+    console.log('CPU: ',cpu.innerText);
+  }
+
+  // Hohlt die Ram daten
+  const memoryGb = document.getElementById('ram');
+  const memoryType = document.getElementById('ramtype');
+  if(memoryGb.innerText!== '' || memoryType.innerText!== '' ) {
+    console.log('RAM: ',memoryGb.innerText, memoryType.innerText);
+  }
+
+  // Hohlt die Grafikkarten infos 
+  const grafik = document.getElementById('grafik');
+  if(grafik.innerText!== '') {
+    console.log('Grafik: \n',grafik.innerText);
+  }
+
+  // Hohlt die Festplatten infos 
+  const festplatte = document.getElementById('laufwerkeName');
+  if(festplatte.innerText!== '') {
+    console.log('Festplatten: \n',festplatte.innerText);
+  }
+
+  // Hohlt die Laufwerks infos 
+  const laufwerke = document.getElementById('laufwerke');
+  if(laufwerke.innerText!== '') {
+    console.log('Laufwerke: \n',laufwerke.innerText);
+  }
+},{once: true});
