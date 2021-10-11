e3e1381f0317c548ab96decbcd801a615f1daa7e
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-11 17:33:19 +0200

    Add Checkbox & Radiobuttons check

--- src/JS/kunde.js
+++ src/JS/kunde.js
@@ -6,7 +6,8 @@ const pfad = require('path');
 //Eventlistener zum Speichern der Daten in JSON
 const kundenSave = document.getElementById('kunde-save');
 kundenSave.addEventListener('click', () => {
-
+  // Cleart die Konsole
+  console.clear();
   // Hohlt Aktuelles Datum
   const heute = document.getElementById('datum');
   if(heute.innerText !== '') {
@@ -40,6 +41,8 @@ kundenSave.addEventListener('click', () => {
     console.log('BenutzerPW: ',benutzerPW.value);
   }
 
+  //#########################################################
+
   //Hohlt die OS daten
   const os = document.getElementById('os');
   if(os.innerText!== '') {
@@ -54,9 +57,9 @@ kundenSave.addEventListener('click', () => {
 
   // Hohlt die Ram daten
   const memoryGb = document.getElementById('ram');
-  const memoryType = document.getElementById('ramtype');
-  if(memoryGb.innerText!== '' || memoryType.innerText!== '' ) {
-    console.log('RAM: ',memoryGb.innerText, memoryType.innerText);
+  //const memoryType = document.getElementById('ramtype');
+  if(memoryGb.innerText!== '') {
+    console.log('RAM: ',memoryGb.innerText);
   }
 
   // Hohlt die Grafikkarten infos 
@@ -76,4 +79,265 @@ kundenSave.addEventListener('click', () => {
   if(laufwerke.innerText!== '') {
     console.log('Laufwerke: \n',laufwerke.innerText);
   }
-},{once: true});
+
+  //#########################################################
+
+  // Hohlt die Windows Updates
+  const winUpdate = document.getElementById('winUpdates');
+  if(winUpdate.value !== '') {
+    console.log('Windows Update: ',winUpdate.value, 'Aktualisierungen');
+  }
+
+  // Hohlt die schadsoftware Funde
+  const schadsoftware = document.getElementById('schadsoftware');
+  if(schadsoftware.value !== '') {
+    console.log('schadsoftware: ',schadsoftware.value,'Funde');
+  }
+
+  // Hohlt die Viren Funde
+  const virenCheck = document.getElementById('virenCheck');
+  if(virenCheck.value !== '') {
+    console.log('virenCheck: ',virenCheck.value,'Funde');
+  }
+
+  // Hohlt die CCleaner Bereinigung 
+  const ccleanerB = document.getElementById('ccleanerB');
+  if(ccleanerB.value !== '') {
+    console.log('ccleanerB: ',ccleanerB.value,'MB');
+  }
+
+  // Hohlt die CCleaner Registry 1
+  const ccleanerR1 = document.getElementById('ccleanerR1');
+  if(ccleanerR1.value !== '') {
+    console.log('ccleanerR1: ',ccleanerR1.value,'Registry Funde');
+  }
+
+  // Hohlt die CCleaner Registry 1
+  const ccleanerR2 = document.getElementById('ccleanerR2');
+  if(ccleanerR2.value !== '') {
+    console.log('ccleanerR2: +',ccleanerR2.value);
+  }
+
+  // Hohlt die CCleaner Registry 1
+  const ccleanerR3 = document.getElementById('ccleanerR3');
+  if(ccleanerR3.value !== '') {
+    console.log('ccleanerR3: +',ccleanerR3.value);
+  }
+
+  // Hohlt die Systembereinigung 1
+  const systembereinigung1 = document.getElementById('1systembereinigung');
+  if(systembereinigung1.value !== '') {
+    console.log('1. systembereinigung',systembereinigung1.value);
+  }
+
+  // Hohlt die Systembereinigung 2
+  const systembereinigung2 = document.getElementById('2systembereinigung');
+  if(systembereinigung2.value !== '') {
+    console.log('2. systembereinigung',systembereinigung2.value);
+  }
+
+  // Hohlt hochdruck checkbox
+  const hochdruck = document.getElementById('hochdruck');
+  if(hochdruck.checked === true) {
+    console.log('Hochdruck ',hochdruck.value);
+  }
+  else {
+    console.log('Keine Hochdruck Reinigung ',hochdruck.checked);
+  }
+
+  // Hohlt Gereinigt checkbox
+  const gereinigt = document.getElementById('gereinigt');
+  if(gereinigt.checked === true) {
+    console.log('Gereinigt ',gereinigt.value);
+  }
+  else {
+    console.log('Nicht Gereinigt ',gereinigt.checked);
+  }
+
+  // Hohlt den Inhalt der Textarea
+  const auffäligkeiten = document.getElementById('auffäligkeiten');
+  if(auffäligkeiten.value !== '') {
+    console.log('Auffäligkeiten ',auffäligkeiten.value);
+  }
+
+  
+  //#########################################################
+
+  // Prüft die RadioButtons => lüftergeräusch
+  const lüftergeräusch = document.getElementsByName('lueftergeraeusch');
+
+  if(lüftergeräusch[0].checked === true) {
+    console.log('Lüftergeräusche ',lüftergeräusch[0].value);
+  }
+  else if(lüftergeräusch[1].checked === true) {
+    console.log('Lüftergeräusche ',lüftergeräusch[1].value);
+  }
+  else {
+    console.log('Lüftergeräusche auswählen');
+  }
+
+  // Prüft die RadioButtons => powerLed
+  const powerLed = document.getElementsByName('powerLed');
+
+  if(powerLed[0].checked === true) {
+    console.log('Power Led ',powerLed[0].value);
+  }
+  else if(powerLed[1].checked === true) {
+    console.log('Power Led ',powerLed[1].value);
+  }
+  else {
+    console.log('Power Led auswählen');
+  }
+
+  // Prüft die RadioButtons => cpuRam
+  const cpuRam = document.getElementsByName('cpuRam');
+
+  if(cpuRam[0].checked === true) {
+    console.log('CPU/Ram ',cpuRam[0].value);
+  }
+  else if(cpuRam[1].checked === true) {
+    console.log('CPU/Ram ',cpuRam[1].value);
+  }
+  else {
+    console.log('CPU/Ram auswählen');
+  }
+
+  // Prüft die RadioButtons => hdSsd
+  const hdSsd = document.getElementsByName('hdSsd');
+
+  if(hdSsd[0].checked === true) {
+    console.log('HD/SSD/SSDSsd ',hdSsd[0].value);
+  }
+  else if(hdSsd[1].checked === true) {
+    console.log('HD/SSD ',hdSsd[1].value);
+  }
+  else {
+    console.log('HD/SSD auswählen');
+  }
+
+  // Prüft die RadioButtons => winAktiv
+  const winAktiv = document.getElementsByName('winAktiv');
+
+  if(winAktiv[0].checked === true) {
+    console.log('Windows Aktiv ',winAktiv[0].value);
+  }
+  else if(winAktiv[1].checked === true) {
+    console.log('Windows Aktiv ',winAktiv[1].value);
+  }
+  else {
+    console.log('Windows Aktiv auswählen');
+  }
+
+  // Prüft die RadioButtons => gerät
+  const gerät = document.getElementsByName('gerät');
+
+  if(gerät[0].checked === true) {
+    console.log('Gerätemanager ',gerät[0].value);
+  }
+  else if(gerät[1].checked === true) {
+    console.log('Gerätemanager ',gerät[1].value);
+  }
+  else {
+    console.log('Gerätemanager auswählen');
+  }
+
+  // Prüft die RadioButtons => frontSound
+  const frontSound = document.getElementsByName('frontSound');
+
+  if(frontSound[0].checked === true) {
+    console.log('FrontSound ',frontSound[0].value);
+  }
+  else if(frontSound[1].checked === true) {
+    console.log('FrontSound ',frontSound[1].value);
+  }
+  else {
+    console.log('FrontSound auswählen');
+  }
+
+  // Prüft die RadioButtons => rearSound
+  const rearSound = document.getElementsByName('rearSound');
+
+  if(rearSound[0].checked === true) {
+    console.log('RearSound ',rearSound[0].value);
+  }
+  else if(rearSound[1].checked === true) {
+    console.log('RearSound ',rearSound[1].value);
+  }
+  else {
+    console.log('RearSound auswählen');
+  }
+
+  // Prüft die RadioButtons => frontUSB
+  const frontUSB = document.getElementsByName('frontUSB');
+
+  if(frontUSB[0].checked === true) {
+    console.log('Front-USB ',frontUSB[0].value);
+  }
+  else if(frontUSB[1].checked === true) {
+    console.log('Front-USB ',frontUSB[1].value);
+  }
+  else {
+    console.log('Front-USB auswählen');
+  }
+
+  // Prüft die RadioButtons => rearUSB
+  const rearUSB = document.getElementsByName('rearUSB');
+
+  if(rearUSB[0].checked === true) {
+    console.log('Rear-USB ',rearUSB[0].value);
+  }
+  else if(rearUSB[1].checked === true) {
+    console.log('Rear-USB ',rearUSB[1].value);
+  }
+  else {
+    console.log('Rear-USB auswählen');
+  }
+
+  // Prüft die RadioButtons => cdDVD
+  const cdDVD = document.getElementsByName('cdDVD');
+
+  if(cdDVD[0].checked === true) {
+    console.log('CD/DVD ',cdDVD[0].value);
+  }
+  else if(cdDVD[1].checked === true) {
+    console.log('CD/DVD ',cdDVD[1].value);
+  }
+  else {
+    console.log('CD/DVD auswählen');
+  }
+
+  // Prüft die RadioButtons => lan
+  const lan = document.getElementsByName('lan');
+
+  if(lan[0].checked === true) {
+    console.log('LAN ',lan[0].value);
+  }
+  else if(lan[1].checked === true) {
+    console.log('LAN ',lan[1].value);
+  }
+  else {
+    console.log('LAN auswählen');
+  }
+
+  // Prüft die RadioButtons => lan
+  const wlan = document.getElementsByName('wlan');
+
+  if(wlan[0].checked === true) {
+    console.log('WLAN ',wlan[0].value);
+  }
+  else if(wlan[1].checked === true) {
+    console.log('WLAN ',wlan[1].value);
+  }
+  else {
+    console.log('WLAN auswählen');
+  }
+
+
+
+
+
+
+
+
+
+});
