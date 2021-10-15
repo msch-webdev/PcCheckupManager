33fb06b688a3fef63bd1b04c21d77350a236a64e
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-15 21:52:14 +0200

    add object for json

--- src/JS/kunde.js
+++ src/JS/kunde.js
@@ -1,8 +1,8 @@
 //const {app, BrowserWindow, ipcRenderer} = require('electron');
 //const fs = require('fs');
 //const pfad = require('path');
-
-
+'use strict';
+let customer = {};
 
 //Eventlistener zum Speichern der Daten in JSON
 const kundenSave = document.getElementById('kunde-save');
@@ -17,21 +17,25 @@ kundenSave.addEventListener('click', () => {
   const heute = document.getElementById('datum');
   if(heute.innerText !== '') {
     console.log('Datum: ',heute.innerText);
+    customer.Date = heute.innerText;
   }
   
   //Hohlt die Auftragsnr.
   const auftragsnr = document.getElementById('auftrag');
   if(auftragsnr.value !== '') {
     console.log('Auftragsnr.: ',auftragsnr.value);
+    customer.Order = auftragsnr.value;
   }
   else {
     err.push(auftragsnr);
   }
 
+
   //Hohlt den Vornamen
   const vorname = document.getElementById('vorname');
   if(vorname.value !== '') {
     console.log('Vorname: ', vorname.value);
+    customer.Vorname = vorname.value;
   }
   else {
     err.push(vorname);
@@ -41,6 +45,7 @@ kundenSave.addEventListener('click', () => {
   const nachname = document.getElementById('nachname');
   if(nachname.value !== '') {
     console.log('Nachname: ',nachname.value);
+    customer.Nachname = nachname.value;
   }
   else {
     err.push(nachname);
@@ -50,6 +55,7 @@ kundenSave.addEventListener('click', () => {
   const benutzerName = document.getElementById('benutzerName');
   if(benutzerName.value !== '') {
     console.log('Benutzername: ',benutzerName.value);
+    customer.BenutzerName = benutzerName.value;
   }
   else {
     err.push(benutzerName);
@@ -59,6 +65,7 @@ kundenSave.addEventListener('click', () => {
   const benutzerPW = document.getElementById('benutzerPW');
   if(benutzerPW.value !== '') {
     console.log('BenutzerPW: ',benutzerPW.value);
+    customer.BenutzerPW = benutzerPW.value;
   }
   else {
     err.push(benutzerPW);
@@ -68,8 +75,20 @@ kundenSave.addEventListener('click', () => {
 
   //Hohlt die OS daten
   const os = document.getElementById('os');
-  if(os.innerText!== '') {
+  if(os.innerText!== '') { 
     console.log('OS: ',os.innerText);
+    
+    let system = os.children[0].children[0].innerText
+    let betriebOs = os.children[0].children[1].innerText
+    let update = os.children[0].children[2].innerText
+    
+    system = system.split(':');
+    betriebOs = betriebOs.split(':');
+    update = update.split(':');
+
+    customer.System = system[1].trim();
+    customer.Os = betriebOs[1].trim();
+    customer.Update = update[1].trim();
   }
   else {
     err.push(os);
@@ -79,6 +98,23 @@ kundenSave.addEventListener('click', () => {
   const motherBoard = document.getElementById('motherBoard');
   if(motherBoard.innerText!== '') {
     console.log('Motherboard: ',motherBoard.innerText);
+
+    let board = motherBoard.children[0].children[0].innerText;
+    let model = motherBoard.children[0].children[1].innerText;
+    let version = motherBoard.children[0].children[2].innerText;
+    let bios = motherBoard.children[0].children[3].innerText;
+
+    board = board.split(':');
+    model = model.split(':');
+    version = version.split(':');
+    bios = bios.split(':');
+   
+
+    customer.board = board[1].trim();
+    customer.model = model[1].trim();
+    customer.version = version[1].trim();
+    customer.bios = bios[1].trim() + bios[2];
+
   }
   else {
     err.push(motherBoard);
@@ -88,6 +124,16 @@ kundenSave.addEventListener('click', () => {
   const cpu = document.getElementById('cpu');
   if(cpu.innerText!== '') {
     console.log('CPU: ',cpu.innerText);
+
+    let cpuModel = cpu.children[0].children[0].innerText;
+    let cores = cpu.children[0].children[1].innerText;
+
+    cpuModel = cpuModel.split(':');
+    cores = cores.split(':');
+
+    customer.cpuModel = cpuModel[1].trim();
+    customer.cores = cores[1].trim();
+
   }
   else {
     err.push(cpu);
@@ -98,6 +144,12 @@ kundenSave.addEventListener('click', () => {
   //const memoryType = document.getElementById('ramtype');
   if(memoryGb.innerText!== '') {
     console.log('RAM: ',memoryGb.innerText);
+
+    let ram = memoryGb.innerText;
+
+    ram = ram.split(':');
+
+    customer.ram = ram[1].trim()
   }
   else {
     err.push(memoryGb);
@@ -107,6 +159,21 @@ kundenSave.addEventListener('click', () => {
   const grafik = document.getElementById('grafik');
   if(grafik.innerText!== '') {
     console.log('Grafik: \n',grafik.innerText);
+
+    let count = grafik.children;
+
+    customer.Gpu = [];
+
+    for (let i = 0; i < count.length; i++) {
+
+      let gpuModel = grafik.children[i].children[0].innerText;
+      let gpuRam = grafik.children[i].children[1].innerText;
+
+      gpuModel = gpuModel.split(':');
+      gpuRam = gpuRam.split(':');
+
+      customer.Gpu[i] = {GPUmodel : gpuModel[1].trim(), GPUram: gpuRam[1].trim()};
+    }
   }
   else {
     err.push(grafik);
@@ -114,8 +181,28 @@ kundenSave.addEventListener('click', () => {
 
   // Hohlt die Festplatten infos 
   const festplatte = document.getElementById('laufwerkeName');
+
   if(festplatte.innerText!== '') {
     console.log('Festplatten: \n',festplatte.innerText);
+
+    let count = festplatte.children;
+    customer.Festplatten = [];
+
+    for (let i = 0; i < count.length; i++) {
+
+      let hdName = festplatte.children[i].children[0].innerText;
+      let hdtype = festplatte.children[i].children[1].innerText;
+      let hdInterface = festplatte.children[i].children[2].innerText;
+
+
+      hdName = hdName.split(':');
+      hdtype = hdtype.split(':');
+      hdInterface = hdInterface.split(':');
+
+      customer.Festplatten[i] = {HDname : hdName[1].trim(), HDtype: hdtype[1].trim(), HDinterface: hdInterface[1].trim()};
+      
+    }
+
   }
   else {
     err.push(festplatte);
@@ -125,6 +212,19 @@ kundenSave.addEventListener('click', () => {
   const laufwerke = document.getElementById('laufwerke');
   if(laufwerke.innerText!== '') {
     console.log('Laufwerke: \n',laufwerke.innerText);
+
+    let count = laufwerke.children;
+    customer.Laufwerke = [];
+
+    for (let i = 0; i < count.length; i++) {
+
+      let lwName = laufwerke.children[i].children[0].innerText;
+      let lwFree = laufwerke.children[i].children[1].innerText;
+      let lwFull = laufwerke.children[i].children[2].innerText;
+
+      customer.Laufwerke[i] = {Path : lwName[0].trim(), Free: lwFree, Full: lwFull};
+
+    }
   }
   else {
     err.push(laufwerke);
@@ -133,18 +233,20 @@ kundenSave.addEventListener('click', () => {
   //#########################################################
 
   // Hohlt die Windows Updates
-  const winUpdate = document.getElementById('winUpdates');
-  if(winUpdate.value !== '') {
-    console.log('Windows Update: ',winUpdate.value, 'Aktualisierungen');
+  const osUpdate = document.getElementById('winUpdates');
+  if(osUpdate.value !== '') {
+    console.log('Windows Update: ',osUpdate.value, 'Aktualisierungen');
+    customer.OsUpdate = osUpdate.value;
   }
   else {
-    err.push(winUpdate);
+    err.push(osUpdate);
   }
 
   // Hohlt die schadsoftware Funde
   const schadsoftware = document.getElementById('schadsoftware');
   if(schadsoftware.value !== '') {
     console.log('schadsoftware: ',schadsoftware.value,'Funde');
+    customer.Schadsoftware = schadsoftware.value;
   }
   else {
     err.push(schadsoftware);
@@ -154,6 +256,7 @@ kundenSave.addEventListener('click', () => {
   const virenCheck = document.getElementById('virenCheck');
   if(virenCheck.value !== '') {
     console.log('virenCheck: ',virenCheck.value,'Funde');
+    customer.VirenCheck = virenCheck.value;
   }
   else {
     err.push(virenCheck);
@@ -163,6 +266,7 @@ kundenSave.addEventListener('click', () => {
   const ccleanerB = document.getElementById('ccleanerB');
   if(ccleanerB.value !== '') {
     console.log('ccleanerB: ',ccleanerB.value,'MB');
+    customer.CcleanerB = ccleanerB.value;
   }
   else {
     err.push(ccleanerB);
@@ -172,6 +276,7 @@ kundenSave.addEventListener('click', () => {
   const ccleanerR1 = document.getElementById('ccleanerR1');
   if(ccleanerR1.value !== '') {
     console.log('ccleanerR1: ',ccleanerR1.value,'Registry Funde');
+    customer.CcleanerR1 = ccleanerR1.value;
   }
   else {
     err.push(ccleanerR1);
@@ -181,6 +286,7 @@ kundenSave.addEventListener('click', () => {
   const ccleanerR2 = document.getElementById('ccleanerR2');
   if(ccleanerR2.value !== '') {
     console.log('ccleanerR2: +',ccleanerR2.value);
+    customer.CcleanerR2 = ccleanerR2.value;
   }
   else {
     err.push(ccleanerR2);
@@ -190,6 +296,7 @@ kundenSave.addEventListener('click', () => {
   const ccleanerR3 = document.getElementById('ccleanerR3');
   if(ccleanerR3.value !== '') {
     console.log('ccleanerR3: +',ccleanerR3.value);
+    customer.CcleanerR3 = ccleanerR3.value;
   }
   else {
     err.push(ccleanerR3);
@@ -199,6 +306,7 @@ kundenSave.addEventListener('click', () => {
   const systembereinigung1 = document.getElementById('1systembereinigung');
   if(systembereinigung1.value !== '') {
     console.log('1. systembereinigung',systembereinigung1.value);
+    customer.Systembereinigung1 = systembereinigung1.value;
   }
   else {
     err.push(systembereinigung1);
@@ -208,6 +316,7 @@ kundenSave.addEventListener('click', () => {
   const systembereinigung2 = document.getElementById('2systembereinigung');
   if(systembereinigung2.value !== '') {
     console.log('2. systembereinigung',systembereinigung2.value);
+    customer.Systembereinigung2 = systembereinigung2.value;
   }
   else {
     err.push(systembereinigung2);
@@ -217,6 +326,7 @@ kundenSave.addEventListener('click', () => {
   const hochdruck = document.getElementById('hochdruck');
   if(hochdruck.checked === true) {
     console.log('Hochdruck ',hochdruck.value);
+    customer.Hochdruck = hochdruck.value;
   }
   else {
     err.push(hochdruck);
@@ -227,6 +337,7 @@ kundenSave.addEventListener('click', () => {
   const gereinigt = document.getElementById('gereinigt');
   if(gereinigt.checked === true) {
     console.log('Gereinigt ',gereinigt.value);
+    customer.Gereinigt = gereinigt.value;
   }
   else {
     err.push(gereinigt);
@@ -237,6 +348,7 @@ kundenSave.addEventListener('click', () => {
   const auffäligkeiten = document.getElementById('auffäligkeiten');
   if(auffäligkeiten.value !== '') {
     console.log('Auffäligkeiten ',auffäligkeiten.value);
+    customer.Auffäligkeiten = auffäligkeiten.value;
   }
   else {
     err.push(auffäligkeiten);
@@ -250,9 +362,11 @@ kundenSave.addEventListener('click', () => {
 
   if(lüftergeräusch[0].checked === true) {
     console.log('Lüftergeräusche ',lüftergeräusch[0].value);
+    customer.Lüftergeräusch = lüftergeräusch[0].value;
   }
   else if(lüftergeräusch[1].checked === true) {
     console.log('Lüftergeräusche ',lüftergeräusch[1].value);
+    customer.Lüftergeräusch = lüftergeräusch[1].value;
   }
   else {
     err.push(lüftergeräusch[0].parentElement);
@@ -264,9 +378,11 @@ kundenSave.addEventListener('click', () => {
 
   if(powerLed[0].checked === true) {
     console.log('Power Led ',powerLed[0].value);
+    customer.PowerLed = powerLed[0].value;
   }
   else if(powerLed[1].checked === true) {
     console.log('Power Led ',powerLed[1].value);
+    customer.PowerLed = powerLed[1].value;
   }
   else {
     err.push(powerLed[0].parentElement);
@@ -277,9 +393,11 @@ kundenSave.addEventListener('click', () => {
   const cpuRam = document.getElementsByName('cpuRam');
 
   if(cpuRam[0].checked === true) {
+    customer.CpuRam = cpuRam[0].value;
     console.log('CPU/Ram ',cpuRam[0].value);
   }
   else if(cpuRam[1].checked === true) {
+    customer.CpuRam = cpuRam[1].value;
     console.log('CPU/Ram ',cpuRam[1].value);
   }
   else {
@@ -291,9 +409,11 @@ kundenSave.addEventListener('click', () => {
   const hdSsd = document.getElementsByName('hdSsd');
 
   if(hdSsd[0].checked === true) {
+    customer.HdSsd = hdSsd[0].value;
     console.log('HD/SSD/SSDSsd ',hdSsd[0].value);
   }
   else if(hdSsd[1].checked === true) {
+    customer.HdSsd = hdSsd[1].value;
     console.log('HD/SSD ',hdSsd[1].value);
   }
   else {
@@ -306,9 +426,11 @@ kundenSave.addEventListener('click', () => {
 
   if(winAktiv[0].checked === true) {
     console.log('Windows Aktiv ',winAktiv[0].value);
+    customer.winAktiv = winAktiv[0].value;
   }
   else if(winAktiv[1].checked === true) {
     console.log('Windows Aktiv ',winAktiv[1].value);
+    customer.winAktiv = winAktiv[1].value;
   }
   else {
     err.push(winAktiv[0].parentElement);
@@ -320,9 +442,11 @@ kundenSave.addEventListener('click', () => {
 
   if(gerät[0].checked === true) {
     console.log('Gerätemanager ',gerät[0].value);
+    customer.Gerät = gerät[0].value;
   }
   else if(gerät[1].checked === true) {
     console.log('Gerätemanager ',gerät[1].value);
+    customer.Gerät = gerät[1].value;
   }
   else {
     err.push(gerät[0].parentElement);
@@ -334,9 +458,11 @@ kundenSave.addEventListener('click', () => {
 
   if(frontSound[0].checked === true) {
     console.log('FrontSound ',frontSound[0].value);
+    customer.FrontSound = frontSound[0].value;
   }
   else if(frontSound[1].checked === true) {
     console.log('FrontSound ',frontSound[1].value);
+    customer.FrontSound = frontSound[1].value;
   }
   else {
     err.push(frontSound[0].parentElement);
@@ -348,9 +474,11 @@ kundenSave.addEventListener('click', () => {
 
   if(rearSound[0].checked === true) {
     console.log('RearSound ',rearSound[0].value);
+    customer.RearSound = rearSound[0].value;
   }
   else if(rearSound[1].checked === true) {
     console.log('RearSound ',rearSound[1].value);
+    customer.RearSound = rearSound[1].value;
   }
   else {
     err.push(rearSound[0].parentElement);
@@ -362,9 +490,11 @@ kundenSave.addEventListener('click', () => {
 
   if(frontUSB[0].checked === true) {
     console.log('Front-USB ',frontUSB[0].value);
+    customer.FrontUSB = frontUSB[0].value;
   }
   else if(frontUSB[1].checked === true) {
     console.log('Front-USB ',frontUSB[1].value);
+    customer.FrontUSB = frontUSB[1].value;
   }
   else {
     err.push(frontUSB[0].parentElement);
@@ -376,9 +506,11 @@ kundenSave.addEventListener('click', () => {
 
   if(rearUSB[0].checked === true) {
     console.log('Rear-USB ',rearUSB[0].value);
+    customer.RearUSB = rearUSB[0].value;
   }
   else if(rearUSB[1].checked === true) {
     console.log('Rear-USB ',rearUSB[1].value);
+    customer.RearUSB = rearUSB[1].value;
   }
   else {
     err.push(rearUSB[0].parentElement);
@@ -390,9 +522,11 @@ kundenSave.addEventListener('click', () => {
 
   if(cdDVD[0].checked === true) {
     console.log('CD/DVD ',cdDVD[0].value);
+    customer.CdDVD = cdDVD[0].value;
   }
   else if(cdDVD[1].checked === true) {
     console.log('CD/DVD ',cdDVD[1].value);
+    customer.CdDVD = cdDVD[1].value;
   }
   else {
     err.push(cdDVD[0].parentElement);
@@ -404,9 +538,11 @@ kundenSave.addEventListener('click', () => {
 
   if(lan[0].checked === true) {
     console.log('LAN ',lan[0].value);
+    customer.Lan = lan[0].value;
   }
   else if(lan[1].checked === true) {
     console.log('LAN ',lan[1].value);
+    customer.Lan = lan[1].value;
   }
   else {
     err.push(lan[0].parentElement);
@@ -418,18 +554,28 @@ kundenSave.addEventListener('click', () => {
 
   if(wlan[0].checked === true) {
     console.log('WLAN ',wlan[0].value);
+    customer.Wlan = wlan[0].value;
   }
   else if(wlan[1].checked === true) {
     console.log('WLAN ',wlan[1].value);
+    customer.Wlan = wlan[1].value;
   }
   else {
     err.push(wlan[0].parentElement);
     console.log('WLAN auswählen');
   }
 
+  console.log(customer);
 
+  const pfad = `src/data/${auftragsnr.value}.json`;
+  
   if(err.length < 1) {
     console.log('alles ok')
+    const jsonData = JSON.stringify(customer, null, 2);
+    fs.writeFile(pfad, jsonData, (err, result) => {
+      if(err) console.log('error', err);
+    });
+
   }
   else {
     console.log('Error Array => ', err);
@@ -438,5 +584,3 @@ kundenSave.addEventListener('click', () => {
   }
 
 });
-
-console.log('test');
