4e80f8ecac4cb34b6dcb01d58a26a6a519e89bb5
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-16 19:23:17 +0200

    minify

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,27 +1,4 @@
-'use strict';
-const {ipcRenderer} = require('electron');
-
-//Hohlt das Aktuelle Datum
-const datum = () => {
-  const heute = document.getElementById('datum');
-  const date = new Date();
-  let monat = date.getMonth() + 1;
-  let tag = date.getDate();
-  let jahr = date.getFullYear();
-  heute.innerText = `${tag}.${monat}.${jahr}`;
-};
-datum();
-
-
-// Eventlistener für Button offnet Kunden Vorschau Window
-const windowCustomer = document.getElementById('windowCustomer');
-
-windowCustomer.addEventListener('click', function() {
-  ipcRenderer.send('openCustomerWindow');
-});
-
-
-
+'use strict';const{ipcRenderer}=require('electron');const datum=()=>{const heute=document.getElementById('datum');const date=new Date();let monat=date.getMonth()+1;let tag=date.getDate();let jahr=date.getFullYear();heute.innerText=`${tag}.${monat}.${jahr}`;};datum();const windowCustomer=document.getElementById('windowCustomer');windowCustomer.addEventListener('click',function(){ipcRenderer.send('openCustomerWindow');});
 
 
 
