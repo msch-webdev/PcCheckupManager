96a4d1c6b2ce40539882f77dbe9b35f3403ce5f3
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-16 18:54:52 +0200

    delete div

--- src/JS/print.js
+++ src/JS/print.js
@@ -15,7 +15,7 @@ ipcRenderer.on('JSONobjekt', (event, arg) => {
   //Todo:offnet & Druckt eine gespeicherte alte datei
   const printWrapper = document.getElementById('print-wrapper');
   printWrapper.innerHTML += `
-  <div>
+  
     <span id="date">Datum: <p>${customer.Date}</p></span><br/><br/>
 
     <span id="auftragsnummer">Auftragsnummer: <p>${customer.Order}</p></span><br/><br/>
@@ -48,21 +48,71 @@ ipcRenderer.on('JSONobjekt', (event, arg) => {
       <h3>Ram:</h3>
       <span id="cpuVendor">Grösse: <p>${customer.ram}</p></span>
     </div>
-
     <div class="gpuCarts">
-      <h3>Grafikkarte:</h3>
-  </div>`;
+        <h3>Grafikkarte:</h3>`;
+    
   
-
-  for (const karte of customer['Gpu']) {
+    for (const karte of customer['Gpu']) {
       printWrapper.innerHTML += `
         <span id="GPUmodel">Model: <p>${karte.GPUmodel}</p></span>&nbsp;&nbsp;
         <span id="GPUram">Ram: <p>${karte.GPUram}</p></span><br/>
-      </div>`;
+      </div><div/>`;
+    }
+
+  printWrapper.innerHTML += `<div class="HD">
+  <h3>Festplatten:</h3>`;
+
+  for (const hd of customer['Festplatten']) {
+    printWrapper.innerHTML += `
+      <span id="HDname">Name: <p>${hd.HDname}</p></span>&nbsp;&nbsp;
+      <span id="HDtype">Model: <p>${hd.HDtype}</p></span>&nbsp;&nbsp;
+      <span id="HDinterface">Cores: <p>${hd.HDinterface}</p></span><br/>
+    </div>`;
   }
 
-  printWrapper.innerHTML += `</div>`;
+  printWrapper.innerHTML += `<div class="Partition">
+  <h3>Partition:</h3>`;
 
+  for (const lw of customer['Laufwerke']) {
+    printWrapper.innerHTML += `
+      <span id="Partition">Name: <p>${lw.Path}</p><p>${lw.Free}</p> frei von <p>${lw.Full}</p></span><br/>
+    </div>`;
+  }
+
+  printWrapper.innerHTML += `
+  <div id="check-container">
+    <div class="arbeiten">
+      <span>Windwos Updates: <p>${customer.OsUpdate}</p> Aktualisierungen</span><br/>
+      <span>Schadsoftware Check: <p>${customer.Schadsoftware}</p> Funde entfernt</span><br/>
+      <span>Viren Check: <p>${customer.VirenCheck}</p> Funde entfernt</span><br/>
+      <span>CCleaner Bereinigung: <p>${customer.CcleanerB}</p> MB</span><br/>
+      <span>CCleaner Registry: <p>${customer.CcleanerR1}</p> Registry Funde <br/> 
+        &nbsp;&nbsp;&nbsp;( 2. Funde <p>${customer.CcleanerR2}</p> - 
+                          3. Funde <p>${customer.CcleanerR3}</p> )
+      </span><br/>
+      <span>1. Systembereinigung: <p>${customer.Systembereinigung1}</p> MB / GB</span><br/>
+      <span>2. Systembereinigung: <p>${customer.Systembereinigung2}</p> MB / GB</span><br/>
+      <span>Gerät innen mit Hochdruck gereinigt: <p>${customer.Hochdruck}</p></span><br/>
+      <span>Gerät außen gereinigt: <p>${customer.Gereinigt}</p></span><br/>
+      <span>Auffäligkeiten (Kurzbericht): <p>${customer.Auffäligkeiten}</p></span><br/>
+    </div>
+
+    <div class="checks">
+    <span>Alle Lüfter drehen Geräuschlos: <p>${customer.Lüftergeräusch}</p></span><br/>
+    <span>Power/HD in Betrieb LED´s funktionieren: <p>${customer.PowerLed}</p></span><br/>
+    <span>CPU und Speicher werden korrekt erkannt: <p>${customer.CpuRam}</p></span><br/>
+    <span>HD/SSD wird korrekt erkannt: <p>${customer.HdSsd}</p></span><br/>
+    <span>Windows ist aktiviert: <p>${customer.winAktiv}</p></span><br/>
+    <span>Gerätemanager zeigt keine Ausrufezeichen: <p>${customer.Gerät}</p></span><br/>
+    <span>Front Sound funktioniert: <p>${customer.FrontSound}</p></span><br/>
+    <span>Rear Sound funktioniert: <p>${customer.RearSound}</p></span><br/>
+    <span>Front USB funktioniert: <p>${customer.FrontUSB}</p></span><br/>
+    <span>Rear USB funktioniert: <p>${customer.RearUSB}</p></span><br/>
+    <span>CD/DVD öffnet und liest Medien: <p>${customer.CdDVD}</p></span><br/>
+    <span>LAN Funktioniert: <p>${customer.Lan}</p></span><br/>
+    <span>WLAN Funktioniert: <p>${customer.Wlan}</p></span><br/>
+    </div>
+  </div>`;
   
 })
 
