1c1173a719b42f4fb53894c14a4c7b5a49901af1
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-10-16 17:05:53 +0200

    add more inormations

--- src/JS/print.js
+++ src/JS/print.js
@@ -16,15 +16,56 @@ ipcRenderer.on('JSONobjekt', (event, arg) => {
   const printWrapper = document.getElementById('print-wrapper');
   printWrapper.innerHTML += `
   <div>
-    <span id="date">Datum: ${customer.Date}</span><br/><br/>
+    <span id="date">Datum: <p>${customer.Date}</p></span><br/><br/>
 
-    <span id="auftragsnummer">Auftragsnummer: ${customer.Order}</span><br/><br/>
+    <span id="auftragsnummer">Auftragsnummer: <p>${customer.Order}</p></span><br/><br/>
 
-    <div class="VnNn"><span id="vorname">Vorname: ${customer.Vorname}</span><span id="nachname">Nachname: ${customer.Nachname}</span></div><br/>
+    <div class="BnPw"><span id="benutzer">Benutzer: <p>${customer.BenutzerName}</p></span><span id="passwort">Passwort: <p>${customer.BenutzerPW}</p></span></div>
+    <hr/>
+    <div class="betriebssystem">
+      <h3>Betriebssystem:</h3>
+      <span id="system">System: <p>${customer.System}</p></span>&nbsp;&nbsp;
+      <span id="os">OS: <p>${customer.Os}</p></span><br/>
+      <span id="os">Update version: <p>${customer.Update}</p></span>
+    </div>
 
-    <div class="BnPw"><span id="benutzer">Benutzer: ${customer.BenutzerName}</span><span id="passwort">Passwort: ${customer.BenutzerPW}</span></div>
+    <div class="motherboard">
+      <h3>Motherboard:</h3>
+      <span id="board">Board: <p>${customer.board}</p></span>&nbsp;&nbsp;
+      <span id="model">Model: <p>${customer.model}</p></span>&nbsp;&nbsp;
+      <span id="version">Version: <p>${customer.version}</p></span><br/>
+      <span id="bios">Bios: <p>${customer.bios}</p></span>
+    </div>
+
+    <div class="cpu">
+      <h3>CPU:</h3>
+      <span id="cpuVendor">Hersteller: <p>${customer.cpuManufacturer}</p></span>&nbsp;&nbsp;
+      <span id="model">Model: <p>${customer.cpuModel}</p></span>&nbsp;&nbsp;
+      <span id="cores">Cores: <p>${customer.cores}</p></span><br/>
+    </div>
+
+    <div class="ram">
+      <h3>Ram:</h3>
+      <span id="cpuVendor">Grösse: <p>${customer.ram}</p></span>
+    </div>
+
+    <div class="gpuCarts">
+      <h3>Grafikkarte:</h3>
   </div>`;
+  
+
+  for (const karte of customer['Gpu']) {
+      printWrapper.innerHTML += `
+        <span id="GPUmodel">Model: <p>${karte.GPUmodel}</p></span>&nbsp;&nbsp;
+        <span id="GPUram">Ram: <p>${karte.GPUram}</p></span><br/>
+      </div>`;
+  }
+
+  printWrapper.innerHTML += `</div>`;
+
+  
 })
+
 
 // druckvorschau
 document.getElementById("drucken").addEventListener("click", function (e) {
